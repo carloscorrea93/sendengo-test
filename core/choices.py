32b2fed003e194361c1b2bab46f4a06bb27a4cc0
Carlos Correa <carlos@truehome.com.mx>
2019-04-14 23:36:42 -0500

    add core choices fields

---
+++ core/choices.py
@@ -0,0 +1,38 @@
+SHIPPEER_REQUIREMENTS_CHOICES = (
+    (0, "Línea de transporte"),
+    (1, "Vehículo"),
+    (2, "Operador"),
+)
+STATUS_CHOICES = (
+    (0, "Por validar"),
+    (1, "Validada"),
+    (2, "Suspendida"),
+)
+VEHICLE_TYPE = (
+    (0, "Sin especificar"),
+    (1, "Camioneta de 1.5 toneladas"),
+    (2, "Camioneta de 3.5 toneladas"),
+    (3, "Camioneta de 5.5 toneladas"),
+    (4, "Rabón con caja seca"),
+    (5, "Rabón con caja refrigerada"),
+    (6, "Torton con caja seca"),
+    (7, "Torton con caja refrigerada"),
+    (9, "Trailer de 48ft con caja seca"),
+    (10, "Trailer de 48ft con caja refrigerada"),
+    (11, "Trailer de 53ft con caja seca"),
+    (12, "Trailer de 53ft con caja refrigerada"),
+ )
+
+VEHICLE_DRIVER_STATUS_CHOICES = (
+    (0, "Por validar"),
+    (1, "Validado"),
+    (2, "Suspendido"),
+)
+
+LICENSE_TYPE_CHOICES = (
+    ('A', 'A'),
+    ('B', 'B'),
+    ('C', 'C'),
+    ('D', 'D'),
+    ('F', 'F'),
+)
